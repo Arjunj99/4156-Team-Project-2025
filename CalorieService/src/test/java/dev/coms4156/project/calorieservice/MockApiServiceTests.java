ccb4bc43e87f91401065b863560b9805c8a76619
Arjunj99 <asj397@nyu.edu>
2025-10-15 02:36:08 -0400

    Added Maven Capabilities to the project.

---
+++ CalorieService/src/test/java/dev/coms4156/project/calorieservice/MockApiServiceTests.java
@@ -0,0 +1,14 @@
+package dev.coms4156.project.calorieservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains the unit tests for the RouteController class.
+ */
+@SpringBootTest
+public class MockApiServiceTests {
+
+}
---
+++ CalorieService/src/test/java/dev/coms4156/project/calorieservice/RouteControllerTests.java
@@ -0,0 +1,15 @@
+package dev.coms4156.project.calorieservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains the unit tests for the RouteController class.
+ */
+@SpringBootTest
+public class RouteControllerTests {
+
+}
